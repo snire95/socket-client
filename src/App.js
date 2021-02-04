89f04a4d02d19fc16f1142666f26af1dacc5fe0a
snire95 <snire995@gmail.com>
2021-02-04 13:53:32 +0200

    4/2

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import chat from './chat.svg';
 import './App.css';
 import massags from './data.js';
 import AllMessags from './AllMassags';
-const SERVER = "http://18.217.20.176:5000";
+import X from './X-icon.png'
+
+// const SERVER = "http://18.217.20.176:5000";
+const SERVER = "http://localhost:8080";
 var socket = socketClient (SERVER);
   socket.on('connection', () => {
         console.log(`I'm connected with the back-end`);
 });
-function App() {
 
+function App() {
   const [messageItems, setMessageItems] = useState(massags);
   const [newMessage, setNewMessage] = useState('')
+  const [Name, setNewName] = useState('')
+
   socket.on('FromAPI', (respons) => {
     var newmessage = JSON.parse(respons)
     console.log('FromAPI');
@@ -21,8 +26,19 @@ function App() {
     if(newmessage.idsocket !== socket.id){
       setMessageItems([...messageItems, newmessage])
     }
-    // setNewMessage('')
   });
+  const closeCate = () => {
+    document.getElementById("boxChat").classList.add('remove');
+    document.getElementById("iconChat").classList.remove('remove');
+    document.getElementById("iconChat").classList.add('flex');
+
+  }
+  const openCate = () => {
+    document.getElementById("iconChat").classList.add('remove');
+    document.getElementById("boxChat").classList.remove('remove');
+    document.getElementById("boxChat").classList.add('flex');
+
+  }
   const hendleSubmit = (e) => {
     e.preventDefault()
     console.log(newMessage);
@@ -30,6 +46,7 @@ function App() {
     console.log( massags.length );
     const NewMessage = {id: new Date().getTime().toString(), 
       idsocket: socket.id,
+      name: Name,
       content: newMessage,
       date: '' + d.getHours()	+ ':' + d.getMinutes()	+ '',
 };
@@ -39,30 +56,57 @@ function App() {
     setMessageItems([...messageItems, NewMessage])
     setNewMessage('')
   }
+  const NewName = (e) => {
+    e.preventDefault()
+    console.log(Name);
+    if(Name !== ''){
+      setNewName(Name);
+      document.getElementById("iconChat").classList.remove('remove');
+      document.getElementById("iconChat").classList.add('flex');
+      document.getElementById("enter-name").classList.remove('flex');
+      document.getElementById("enter-name").classList.add('remove');
+    
+    }
+    alert(`hi ${Name}`)
+  }
+
     return (
       <main>
-        <div  className = "iconChat">
-          <img src ={chat} alt="" />
-        </div>
-      
-          <div className = "boxChat"> 
-            <ol className = "messages-list">
-            <AllMessags message ={messageItems}/>
-            </ol>
-            {/* <div className="x"> */}
-            <form className="x" onSubmit= {hendleSubmit}>
+        <div className="enter-name flex" id = "enter-name">
+          <p >Please enter your name</p>
+          <form className="center" onSubmit= {NewName}>
             <button type ='submit' className= "SendMessage"> Send</button>
             <input 
-            type='text' 
-            className ='boxText' 
-            value={newMessage} 
-            onChange={(e) => setNewMessage(e.target.value)}/>
-            </form>
-            </div> 
-          {/* </div>      */}
+              type='text' 
+              className ='boxText' 
+              value={Name} 
+              onChange={(e) => setNewName(e.target.value)}/>
+          </form>
+        </div>
+        <div className = "iconChat remove" id = "iconChat" >
+          <img src ={chat} alt="" onClick = {openCate} />
+        </div>
+        <div className = "boxChat remove" id = "boxChat"> 
+        <div className = "top-strip">
+          <img src= {X} alt="" className = "X" onClick = {closeCate}></img>
+          <div className = "enabel">Enabling guests : </div>
+
+        </div>
+          <ol className = "messages-list">
+            <AllMessags message ={messageItems}/>
+          </ol>
+          <form className="x" onSubmit= {hendleSubmit}>
+            <button type ='submit' className= "SendMessage"> Send</button>
+            <input 
+              type='text' 
+              className ='boxText' 
+              value={newMessage} 
+              onChange={(e) => setNewMessage(e.target.value)}/>
+          </form>
+        </div> 
       </main>
     );
-    };
+};
 
 
 export default App;
